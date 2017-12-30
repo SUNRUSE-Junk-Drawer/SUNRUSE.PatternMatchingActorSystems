cab2a8f3d91509217d53a2d5638ce6c3ea1b1743
jameswilddev <jameswilddev@outlook.com>
2017-12-30 16:18:47 +0000

    Added the IntegerLiteral expression.

---
+++ SUNRUSE.PatternMatchingActorSystems.Shared.Tests/Expressions/IntegerLiteralTests.cs
@@ -0,0 +1,27 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace SUNRUSE.PatternMatchingActorSystems.Shared.Expressions
+{
+    public sealed class IntegerLiteralTests
+    {
+        [Fact]
+        public void ToExpressionBody()
+        {
+            var expression = new IntegerLiteral(235455);
+
+            var expressionBody = expression.ToExpressionBody();
+
+            var cast = Expression.Convert(expressionBody, typeof(int));
+            var wrapped = Expression.Lambda<Func<int>>(cast);
+            var compiled = wrapped.Compile();
+            var evaluated = compiled();
+            Assert.Equal(235455, evaluated);
+        }
+    }
+}
---
+++ SUNRUSE.PatternMatchingActorSystems.Shared/Expressions/IntegerLiteral.cs
@@ -0,0 +1,29 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SUNRUSE.PatternMatchingActorSystems.Shared.Expressions
+{
+    /// <summary>A <see cref="IExpression" /> which returns a constant <see langword="int" />.</summary>
+    public sealed class IntegerLiteral : IExpression
+    {
+        /// <summary>The <see cref="int" /> to return.</summary>
+        public readonly int Integer;
+
+        /// <inheritdoc />
+        public IntegerLiteral(int integer)
+        {
+            Integer = integer;
+        }
+
+        /// <inheritdoc />
+        public Expression ToExpressionBody()
+        {
+            return Expression.Constant(Integer);
+        }
+    }
+}
